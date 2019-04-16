8fc627a696f4b8d28929d74c8916f2ab4a432cb4
Ben Pate <ben@JARVIS.local>
2019-04-16 12:27:58 -0600

    Improving middleware error tests

--- middleware_test.go
+++ middleware_test.go
@@ -1,6 +1,7 @@
 package remote
 
 import (
+	"net/http"
 	"testing"
 
 	"github.com/benpate/derp"
@@ -21,19 +22,43 @@ func TestMiddleware(t *testing.T) {
 	Get(server.URL).Use(middleware).Send()
 }
 
-func TestMiddlewareError(t *testing.T) {
+func TestMiddlewareErrors(t *testing.T) {
 
 	// Create a simple middleware to write the transaction to stdout
-	middleware := Middleware{
+	configError := Middleware{
 		Config: func(transaction *Transaction) *derp.Error {
 			return derp.New("Middleware.Config", "Error Running Middleware", 0, nil).Report()
+		},
+	}
+
+	// Create a simple middleware to write the transaction to stdout
+	requestError := Middleware{
+		Request: func(request *http.Request) *derp.Error {
+			return derp.New("Middleware.Request", "Error Running Middleware", 0, nil).Report()
+		},
+	}
+
+	// Create a simple middleware to write the transaction to stdout
+	responseError := Middleware{
+		Response: func(response *http.Response, body *[]byte) *derp.Error {
+			return derp.New("Middleware.Response", "Error Running Middleware", 0, nil).Report()
 		},
 	}
 
 	server := echoBodyServer()
 
 	// We're EXPECTING an error, so if there's no error, then that's BAD.
-	if err := Get(server.URL).Use(middleware); err == nil {
+	if err := Get(server.URL).Use(configError); err == nil {
+		t.Fail()
+	}
+
+	// We're EXPECTING an error, so if there's no error, then that's BAD.
+	if err := Get(server.URL).Use(requestError); err == nil {
+		t.Fail()
+	}
+
+	// We're EXPECTING an error, so if there's no error, then that's BAD.
+	if err := Get(server.URL).Use(responseError); err == nil {
 		t.Fail()
 	}
 }
