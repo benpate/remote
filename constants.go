55d91dc8fe957687eef99bf9a5e84dd5f9113b39
Ben Pate <ben@pate.org>
2023-08-09 09:34:54 -0600

    adding constant for ld+json

--- constants.go
+++ constants.go
@@ -19,7 +19,12 @@ const ContentTypeHTML = "text/html"
 // ContentTypeJSON is the standard MIME Type for JSON content
 const ContentTypeJSON = "application/json"
 
+// ContentTypeJSONLD is the standard MIME Type for JSON-LD content
+// https://en.wikipedia.org/wiki/JSON-LD
+const ContentTypeJSONLD = "application/ld+json"
+
 // ContentTypeJSONFeed is the standard MIME Type for JSON Feed content
+// https://en.wikipedia.org/wiki/JSON_Feed
 const ContentTypeJSONFeed = "application/feed+json"
 
 // ContentTypeJSONResourceDescriptor is the standard MIME Type for JSON Resource Descriptor content
